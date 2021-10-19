c91ad7ef24bc778dc6d668c167ff5174e9069416
jsantore <jsantore@bridgew.edu>
2021-10-19 10:49:29 -0400

    At end of class on Tuesday Oct 19. Need to finish button handler

---
+++ src/main/java/bsu/comp152/calctuesthurs/Calcwindow.java
@@ -0,0 +1,17 @@
+package bsu.comp152.calctuesthurs;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Calcwindow {
+    @FXML
+    private TextField numberDisplay;
+
+    @FXML
+    public void numberButtonPushed(ActionEvent event){
+        var currentText = numberDisplay.getText();
+        var currentNumberText = event.getSource().toString();
+        var newText = currentText+currentNumberText;
+    }
+}
