c9f843f4515575a3b9f6084ebf654a2d37a19023
jsantore <jsantore@bridgew.edu>
2021-10-21 10:43:27 -0400

    End of class Oct 21

--- src/main/java/bsu/comp152/calctuesthurs/Calculator.java
+++ src/main/java/bsu/comp152/calctuesthurs/Calculator.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Calculater extends Application {
+public class Calculator extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Calculater.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Calculator.class.getResource("calcwindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        stage.setTitle("Comp152 Calculator");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/bsu/comp152/calctuesthurs/Calcwindow.java
+++ src/main/java/bsu/comp152/calctuesthurs/Calcwindow.java
@@ -2,16 +2,40 @@ package bsu.comp152.calctuesthurs;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Calcwindow {
+    private int storedNumber;
     @FXML
     private TextField numberDisplay;
 
     @FXML
     public void numberButtonPushed(ActionEvent event){
         var currentText = numberDisplay.getText();
-        var currentNumberText = event.getSource().toString();
+        var ButtonPressed = (Button)event.getSource();
+        var currentNumberText = ButtonPressed.getText();
         var newText = currentText+currentNumberText;
+        numberDisplay.setText(newText);
+    }
+
+    @FXML
+    public void addButtonPressed(){
+        var numbertext = numberDisplay.getText();
+        storedNumber = Integer.parseInt(numbertext);
+        numberDisplay.clear();
+    }
+
+    @FXML
+    public void equalButtonPressed(){
+        var currentNumberText = numberDisplay.getText();
+        var currentNumber = Integer.parseInt(currentNumberText);
+        var sum = currentNumber + storedNumber;
+        numberDisplay.setText(""+sum);
+    }
+
+    @FXML
+    public void clearButtonPressed(){
+        numberDisplay.setText("");
     }
 }
